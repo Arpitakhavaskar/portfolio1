705333f8275e9ed45a6b3cb7e94a12e298ab7452
Arpita khavaskar <163789140+Arpitakhavaskar@users.noreply.github.com>
2024-06-08 22:57:18 +0530

    Add files via upload

---
+++ src/components/About.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Arpita khavaskar, a passionate Web developer with a keen eye for Frontend
+          developer . With a background in IT, I strive to create impactful and
+          visually stunning Software solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          Master of Computer Application (MCA)(Pursuing)
+              G H Raisoni engineering college ,nagpur Maharashtra..
+            
+               Bachelor of computer Application (BCA)
+              Shri Shivaji Science College,Nagpur
+        </span>
+        
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Reactjs,Html,Css,Dbms,operating system,  javascript,data structure and algorithm using cpp,
+         Tech Stack : Mern (MonogoDB, ExpressJs,Rectjs,mysql)
+
+        </span>
+        <br />
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+        Hackerrank Rank 5-star golden badge in cpp language
+        Hackerrank Rank 4 -star in mysql
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative [Your Field] solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
